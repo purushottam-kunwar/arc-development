cb72edd7fb16f8f61d33c889e1f266345165d3de
pk <pipalpurus@gmail.com>
2021-06-07 21:25:47 +0545

    update style

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,18 +1,18 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Typography from '@material-ui/core/Typography';
 
 function ElevationScroll(props) {
   const { children } = props;
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0,
+    threshold: 0
   });
 
   return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
+    elevation: trigger ? 4 : 0
   });
 }
 
