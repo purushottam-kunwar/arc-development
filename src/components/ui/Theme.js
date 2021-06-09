66acd69a33c73f2c0fa686081f8f7afcbfdbad8e
pk <pipalpurus@gmail.com>
2021-06-09 08:33:03 +0545

    using tabs

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -17,8 +17,11 @@ export default createMuiTheme({
     }
   },
   typography: {
-    h3: {
-      fontWeight: '300'
+    tab: {
+      fontFamily: 'Roleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1rem'
     }
   }
 });
