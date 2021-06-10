057af7e7ee5e86a5858ecdeea5720358efe4056f
pk <pipalpurus@gmail.com>
2021-06-10 08:36:39 +0545

    active tab refresh fixes

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em'
   },
   logo: {
-    height: '7em'
+    height: '8em'
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -44,6 +44,12 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '50px',
     marginRight: '25px',
     height: '45px'
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
   }
 }));
 
@@ -56,12 +62,36 @@ export default function Header(props) {
     setValue(value);
   };
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  });
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img alt="Company Logo" src={logo} className={classes.logo} />
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img alt="Company Logo" src={logo} className={classes.logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChangeTab}
